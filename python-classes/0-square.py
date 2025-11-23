52df877916a1f018bdd1e815369bcf59083493f9
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-23 11:43:19 +0000

    ff

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"'this module a square class"'
 class Square():
+    """asdfghj"""
     def __init__(self,size=0):
        if not isinstance(size,int):
            raise TypeError("fuck")
